94e937d4ee35d1d7229c46cadd323f0ba12403b4
Thurshani <thurshanin.20@uom.lk>
2025-06-13 19:01:22 +0530

    new

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -1,53 +1,51 @@
 import User from "../models/User.js";
 import { Webhook } from "svix";
+import connectDB from "../configs/db.js";
 
-const clerkWebhooks=async (req,res)=>{
-    try{
-        const whook=new Webhook(process.env.CLERK_WEBHOOK_SECRET)
-        
-        //Gettings headers
-        const headers={
-            "svix-id":req.headers["svix-id"],
-            "svix-timestamp":req.headers["svix-timestamp"],
-            "svix-signature":req.headers["svix-signature"],
-        } ;
+connectDB(); // Connect MongoDB once per function call
 
-        //Verifying Headers
-        await whook.verify(JSON.stringify(req.body),headers)
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method Not Allowed" });
+  }
 
-        //Geting Data from request body
-        const{data,type}=req.body
+  try {
+    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
-        const userData={
-            _id:data.id,
-            email:data.email_addresses[0].email_addresses,
-            username:data.first_name+ " " +data.last_name,
-            image:data.image_url,
-        }
+    const headers = {
+      "svix-id": req.headers["svix-id"],
+      "svix-timestamp": req.headers["svix-timestamp"],
+      "svix-signature": req.headers["svix-signature"],
+    };
 
-        //switch cases for different events
-        switch (type) {
-            case "user.created":{
-                await User.create(userData);
-                break;
-            }
-               case "user.updated":{
-                await User.findByIdUpdate(data.id,userData);
-                break;
-            }   
-               case "user.deleted":{
-                await User.findByIdDelete(data.id);
-                break;
-            } 
-        
-            default:
-                break;
-        }
-        res.json({suceess:true,messsage:"Webhook Received"})
-    }catch(error){
-        console.log(error.messsage)
-        res.json({suceess:false,messsage:error.messsage});
+    await whook.verify(JSON.stringify(req.body), headers);
+
+    const { data, type } = req.body;
+
+    const userData = {
+      _id: data.id,
+      email: data.email_addresses[0].email_address, // fix typo (email_addresses is array of objects)
+      username: data.first_name + " " + data.last_name,
+      image: data.image_url,
+    };
+
+    switch (type) {
+      case "user.created":
+        await User.create(userData);
+        break;
+      case "user.updated":
+        await User.findByIdAndUpdate(data.id, userData);
+        break;
+      case "user.deleted":
+        await User.findByIdAndDelete(data.id);
+        break;
+      default:
+        break;
     }
+
+    res.json({ success: true, message: "Webhook Received" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ success: false, message: error.message });
+  }
 }
-export default clerkWebhooks;
-    
--- server/models/User.js
+++ server/models/User.js
@@ -1,15 +1,14 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 
-const UserSchema=mongoose.SchemaO({
-    _id:{type:String,required:true},
-    username:{type:String,required:true},
-    email:{type:String,required:true},
-    image:{type:String,required:true},
-    role:{type:String,enum:["user","hotelOwner"],default:"user"},
-    recentSearchCities:[{type:String,required:true}],
-    
-},{timestamps:true});
+const UserSchema = mongoose.Schema({
+    _id: { type: String, required: true },
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    image: { type: String, required: true },
+    role: { type: String, enum: ["user", "hotelOwner"], default: "user" },
+    recentSearchCities: [{ type: String, required: true }],
+}, { timestamps: true });
 
-const User=mongoose.model("User",userSchema);
+const User = mongoose.model("User", UserSchema);
 
 export default User;
