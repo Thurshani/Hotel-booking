af6633cb41acb221e176bdd10a782669b78a5c17
Thurshani <thurshanin.20@uom.lk>
2025-06-15 23:03:47 +0530

    15/6-true

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -4,7 +4,7 @@ import { Webhook } from "svix"
 const clerkWebhooks= async (req,res) => {
     try {
         const whook=new Webhook(process.env.CLERK_WEBHOOK_SECRET)
-        const header={
+        const headers={
             "svix-id":req.headers[ "svix-id"],
             "svix-timestamp":req.headers[ "svix-timestamp"],
             "svix-signature":req.headers[ "svix-signature"],
@@ -17,9 +17,9 @@ const clerkWebhooks= async (req,res) => {
 
         const userData={
             _id:data.id,
-            email:data.email_addresses[0].email_addresses,
+            email:data.email_addresses[0].email_address,
             username:data.first_name+" "+data.last_name,
-            image:data.image_url,
+           
         }
 
         switch (type) {
--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json())
 app.use(clerkMiddleware())
 
-app.use("api/clerk",clerkWebhooks);
+app.use("/api/clerk",clerkWebhooks);
 
 app.get("/", (req, res) => res.send("API is working"));
 
